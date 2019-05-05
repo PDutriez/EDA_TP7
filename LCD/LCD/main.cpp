42e3d9456b035b6e5b6e0b1cb206028199be19c6
Ricardo0623 <42210980+Ricardo0623@users.noreply.github.com>
2019-05-05 20:44:26 -0300

    commit

--- LCD/LCD/main.cpp
+++ LCD/LCD/main.cpp
@@ -7,13 +7,24 @@ int main(void)
 	FT_HANDLE handle;
 	const char * displayname = "EDA LCD 2 B";
 	lcdInit(displayname, handle);
-	const char text[] = "Phil, funciona!";
+	const char text[] = "Phil, funciona!AAAA";
 	
 	lcdWriteByte(handle, 0x0F, RS_INST);
 	for (int i = 0; i < 15; i++)
 	{
 		lcdWriteByte(handle, text[i], RS_DATA);
 	}
+	getchar();
+	for (int i = 0; i < (5*8-15); i++)
+	{
+		lcdWriteByte(handle, 0x14, RS_INST);
+	}
+	getchar();
+	for (int i = 15; i<19; i++)
+	{
+		lcdWriteByte(handle, text[i], RS_DATA);
+	}
 	printf("press enter\n");
 	getchar();
+	FT_Close(handle);
 }
